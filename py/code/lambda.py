735c539383bade781159c0ba58276b34001e2b32
Jonathan Saddler <1129384+jazad136@users.noreply.github.com>
2026-05-19 10:44:21 -0400

    Update Lambda function

--- py/code/lambda.py
+++ py/code/lambda.py
@@ -3,8 +3,32 @@ import os
 bucket_name = os.environ['MESSAGES_BUCKET']
 
 def handler(event, context):
+    # print(bucket_name)
+    # return {
+        # 'statusCode': 200,
+        # 'body': json.dumps('Hello from Lambda')
+    # }
+    method = event['httpMethod']
+    if method == 'GET':
+        return get_messages()
+    if method == 'POST':
+        return post_message(event)
+    else: 
+        return {
+            'statusCode': 405,
+            'body' : json.dumps('Method Not Allowed')
+        }
+
+def get_messages():
     print(bucket_name)
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda')
+        'body': json.dumps('Hello from get')
+    }
+
+def post_message(event):
+    print(bucket_name)
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from post')
     }
